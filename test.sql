e1b7273dae7ab2a66d13f0dd475d33cd8c400d68
LosRebelos <95313998+LosRebelos@users.noreply.github.com>
2022-05-16 18:01:23 +0200

    2nd commit

--- test.sql
+++ test.sql
@@ -1,50 +1,50 @@
-WITH category_price_per_year AS (
-	SELECT
-		YEAR(cp.date_from) AS date_year,
-		cp.category_code,
-		cpc.name,
-		cp.value AS avg_value
-	FROM czechia_price cp
-	JOIN czechia_price_category cpc
-	  ON cp.category_code = cpc.code
-	GROUP BY 
-		YEAR(date_from), 
-		category_code
-	ORDER BY 
-		cpc.name, 
-		YEAR(cp.date_from)
-),
-	payroll_avg_per_year AS (
-	SELECT
-		cp.payroll_year,
-		industry_branch_code,
-		cpib.name AS industry_name,
-		avg(value) AS avg_payroll
-	FROM czechia_payroll cp
-	JOIN czechia_payroll_industry_branch cpib
-  	  ON cp.industry_branch_code = cpib.code
-	WHERE TRUE 
-	  AND value_type_code = '5958'
-	  AND calculation_code = '200'
-	GROUP BY payroll_year, industry_branch_code
-),
-	base AS (
-	SELECT
-		*
-	FROM payroll_avg_per_year papy
-	JOIN category_price_per_year cppy
-	  ON papy.payroll_year = cppy.date_year
-	ORDER BY 
-		papy.payroll_year,
-		papy.industry_branch_code
-)
-	SELECT
-		payroll_year AS 'Year',
-		industry_branch_code,
-		industry_name,
-		avg_payroll,
-		name,
-		avg_value AS goods_price
-	FROM base;
-
+CREATE OR REPLACE TABLE NEWONE AS
+	WITH category_price_per_year AS (
+		SELECT
+			YEAR(cp.date_from) AS date_year,
+			cp.category_code,
+			cpc.name,
+			cp.value AS avg_value
+		FROM czechia_price cp
+		JOIN czechia_price_category cpc
+		  ON cp.category_code = cpc.code
+		GROUP BY 
+			YEAR(date_from), 
+			category_code
+		ORDER BY 
+			cpc.name, 
+			YEAR(cp.date_from)
+	),
+		payroll_avg_per_year AS (
+		SELECT
+			cp.payroll_year,
+			industry_branch_code,
+			cpib.name AS industry_name,
+			avg(value) AS avg_payroll
+		FROM czechia_payroll cp
+		JOIN czechia_payroll_industry_branch cpib
+	  	  ON cp.industry_branch_code = cpib.code
+		WHERE TRUE 
+		  AND value_type_code = '5958'
+		  AND calculation_code = '200'
+		GROUP BY payroll_year, industry_branch_code
+	),
+		base AS (
+		SELECT
+			*
+		FROM payroll_avg_per_year papy
+		JOIN category_price_per_year cppy
+		  ON papy.payroll_year = cppy.date_year
+		ORDER BY 
+			papy.payroll_year,
+			papy.industry_branch_code
+	)
+		SELECT
+			payroll_year AS 'Year',
+			industry_branch_code,
+			industry_name,
+			avg_payroll,
+			name,
+			avg_value AS goods_price
+		FROM base;
 
